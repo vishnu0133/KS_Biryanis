734bbc214d09b70fffd539de9fa1bd227244651f
vishnu0133 <vishnupriyadonga@lewisu.edu>
2024-05-05 14:32:17 -0500

    commit

---
+++ KSPB_Restaurant/KSPB_Restaurant/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.home_view, name='home'),
+    path('contact/', views.contact, name='contact'),
+]
+
---
+++ KSPB_Restaurant/KSPB_Restaurant/views.py
@@ -0,0 +1,48 @@
+# views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')  # Redirect to the homepage after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to home page after successful login
+        else:
+            # Handle invalid login credentials
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Redirect to login page after logout
+
+@login_required
+def home_view(request):
+    username = request.user.username
+    return render(request, 'home.html', {'username': username})
+
+    
+def contact(request):
+    return render(request, 'contact.html')
